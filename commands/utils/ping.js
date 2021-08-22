10df7a7233da8a126eb1c504c6ad29b23c1528ca
Sebola3461 <mixvideostubecontato@gmail.com>
2021-08-22 19:36:10 -0300

    Add ping command

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -1,3 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
 exports.run = (message, args) => {
-    message.channel.send("banana1");
+    let msgtime1 = message.createdTimestamp;
+    message.channel.send(':purple_heart: :ping_pong: Pinging...')
+        .then((msg) => {
+            ping = msg.createdTimestamp - msgtime1;
+            const embed = new MessageEmbed()
+            module.exports = (embed)
+                .setColor(`#FFB7E0`)
+                .setDescription(
+                    ":purple_heart: :ping_pong: Pong! bot's ping is `" + ping + 'ms`.'
+                );
+            msg.channel.send(embed);
+            message.delete();
+        });
 }
