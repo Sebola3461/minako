275eb70533a69b54e34078272e03e97ff121e89d
Sebola3461 <mixvideostubecontato@gmail.com>
2021-08-30 10:05:06 -0300

    Fix some errors in eval.js

--- commands/utils/eval.js
+++ commands/utils/eval.js
@@ -9,24 +9,35 @@ let devs = ["556639598172962818", "215330105411633154"]
 
 exports.run = async(message, args, bot) => {
     args = args.join(" ").slice(args[0].length + 1) // * Remove the command name of the string
+
     if (args == "") return;
     if (!devs.includes(message.author.id)) return;
+
+    let evaled;
     try {
-        let evaluate = eval(args);
-        evaluate.then(r => {
-            let embed = new MessageEmbed()
-                .setTitle("Eval")
-                .setDescription(r)
-                .setColor("#09f47b")
-
-            message.channel.send(embed)
-        })
-    } catch (e) {
-        let embed = new MessageEmbed()
-            .setTitle("Eval Error")
-            .setDescription(e)
-            .setColor("#f40940")
-
-        message.channel.send(embed)
+        evaled = await eval(args);
+        evaled = require('util').inspect(evaled, { depth: -1 });
+        message.channel.send({
+            embed: {
+                title: "EVAL",
+                color: "#09f47b",
+                fields: [{
+                        name: "Input:",
+                        value: "`" + args + "`"
+                    },
+                    {
+                        name: "Result:",
+                        value: "`" + evaled + "`"
+                    }
+                ]
+            }
+        });
+    } catch (error) {
+        message.reply({
+            embed: {
+                color: "#f40940",
+                description: "🖨️ - __**Terminal**__\n`" + error + "`"
+            }
+        });
     }
 }
