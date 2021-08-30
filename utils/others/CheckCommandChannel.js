61f66ca238c5cf35f3cedd424f44395dd19626a9
Sebola3461 <mixvideostubecontato@gmail.com>
2021-08-30 09:58:38 -0300

    Fix command channel permissions

--- utils/others/CheckCommandChannel.js
+++ utils/others/CheckCommandChannel.js
@@ -5,6 +5,10 @@ exports.channel = (message) => {
     const db = MinakoDatabase.guilds.getGuild(message.guild.id);
     const notAllowedMessage = db.moderation["channel_not_allowed_message"];
 
+    if (message.member.permissions.has("MANAGE_MESSAGES") | message.member.permissions.has("MANAGE_CHANNELS") | message.member.permissions.has("ADMIN")) return {
+        code: 200
+    }
+
     if (!db.moderation["channels_allowed"].includes(message.channel.id) && db.moderation["channels_allowed"] != "") {
         return {
             code: 400,
